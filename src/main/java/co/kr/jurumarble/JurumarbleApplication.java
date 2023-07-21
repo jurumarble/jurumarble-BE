c80f53854b56b4c04285ef567c1689f589994b28
alsduq1117 <alsduq1117@naver.com>
2023-07-21 20:43:38 +0900

    Initial commit

---
+++ src/main/java/co/kr/jurumarble/JurumarbleApplication.java
@@ -0,0 +1,13 @@
+package co.kr.jurumarble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurumarbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurumarbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/jurumarble/JurumarbleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.jurumarble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurumarbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
