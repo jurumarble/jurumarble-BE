a29037763e9856c89ef1732f64e10068333e0a06
alsduq1117 <alsduq1117@naver.com>
2023-07-22 19:49:06 +0900

    fix : 502 nginx 오류

--- src/main/java/co/kr/jurumarble/EbHealthCheck.java
+++ src/main/java/co/kr/jurumarble/EbHealthCheck.java
@@ -8,6 +8,12 @@ public class EbHealthCheck {
 
     @GetMapping("/")
     public String check() {
-        return "<h1>idle-backend 스웨거 주소: http://jurumarble-eb-env.eba-psdd54jg.ap-northeast-2.elasticbeanstalk.com/swagger-ui/index.html </h1>";
+        return "<h1>스웨거 주소: http://jurumarble-eb-env.eba-psdd54jg.ap-northeast-2.elasticbeanstalk.com/swagger-ui/index.html </h1>";
     }
+
+    @GetMapping("/api/swagger-check")
+    public String swagger() {
+        return "<h1>test</h1>";
+    }
+
 }
