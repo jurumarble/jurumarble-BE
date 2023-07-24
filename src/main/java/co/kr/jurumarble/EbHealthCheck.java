ba5607c13104c61d0298e3194634c4093a3cd9da
alsduq1117 <alsduq1117@naver.com>
2023-07-25 02:28:42 +0900

    fix : 메인페이지 dev,prod 환경 별 redirect url 설정

--- src/main/java/co/kr/jurumarble/EbHealthCheck.java
+++ src/main/java/co/kr/jurumarble/EbHealthCheck.java
@@ -1,17 +1,22 @@
 package co.kr.jurumarble;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@RequiredArgsConstructor
+@Controller
 public class EbHealthCheck {
+
+    @Value("${swagger.url}")
+    private String swaggerUrl;
 
     @GetMapping("/")
     public RedirectView redirectToSwagger() {
-        String swaggerUrl = "http://jurumarble-env.eba-tv8jafay.ap-northeast-2.elasticbeanstalk.com/swagger-ui/index.html";
         return new RedirectView(swaggerUrl);
     }
     @GetMapping("/api/swagger-check3")
