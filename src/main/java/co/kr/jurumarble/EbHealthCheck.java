1355c9e061ed8334b3a3437abc31257aa578fa6f
alsduq1117 <alsduq1117@naver.com>
2023-07-25 01:10:14 +0900

    fix : 메인페이지 redirect url 설정

--- src/main/java/co/kr/jurumarble/EbHealthCheck.java
+++ src/main/java/co/kr/jurumarble/EbHealthCheck.java
@@ -1,5 +1,7 @@
 package co.kr.jurumarble;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -8,9 +10,10 @@ import org.springframework.web.servlet.view.RedirectView;
 public class EbHealthCheck {
 
     @GetMapping("/")
-    public RedirectView redirectToSwagger() {
+    public ResponseEntity<?> redirectToSwagger() {
         String swaggerUrl = "http://jurumarble-env.eba-tv8jafay.ap-northeast-2.elasticbeanstalk.com/swagger-ui/index.html";
-        return new RedirectView(swaggerUrl);
+        // 200(OK) 상태 코드로 리다이렉트 응답을 반환
+        return ResponseEntity.status(200).header("Location", swaggerUrl).build();
     }
 
     @GetMapping("/api/swagger-check3")
