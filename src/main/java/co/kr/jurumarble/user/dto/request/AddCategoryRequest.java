934eb61cce5556d8cee15c900a785e815cc607a5
jonghyunhub <chuwhdgus@gmail.com>
2023-07-26 16:07:01 +0900

    feat : UserController request 객체 추가

---
+++ src/main/java/co/kr/jurumarble/user/dto/request/AddCategoryRequest.java
@@ -0,0 +1,23 @@
+package co.kr.jurumarble.user.dto.request;
+
+import kr.co.chooz.user.dto.AddUserCategory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddCategoryRequest {
+
+    @NotNull(message = "categories는 null 일 수 없습니다.")
+    private List<String> categories;
+
+    public AddUserCategory toAddUserCategory() {
+        return new AddUserCategory(categories);
+    }
+
+}
