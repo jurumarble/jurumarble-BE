8d4b2ea9f3f9e269f6c42623cb01b21a89e43a3e
wellbeing-dough <kdw990202@naver.com>
2023-07-27 18:41:11 +0900

    refact : 필요없는 주석 제거

--- src/main/java/co/kr/jurumarble/vote/repository/VoteRepository.java
+++ src/main/java/co/kr/jurumarble/vote/repository/VoteRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
