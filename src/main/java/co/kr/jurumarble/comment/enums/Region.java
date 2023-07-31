18b681d1606db7d074c5219d8df45dfe0506b285
alsduq1117 <alsduq1117@naver.com>
2023-07-31 23:53:23 +0900

    feat : 지역 enum 추가

---
+++ src/main/java/co/kr/jurumarble/comment/enums/Region.java
@@ -0,0 +1,37 @@
+package co.kr.jurumarble.comment.enums;
+
+public enum Region {
+    SEOUL(1, "서울"),
+    INCHEON(2, "인천"),
+    DAEJEON(3, "대전"),
+    DAEGU(4, "대구"),
+    GWANGJU(5, "광주"),
+    BUSAN(6, "부산"),
+    ULSAN(7, "울산"),
+    SEJONG(8, "세종특별자치시"),
+    GYEONGGI(31, "경기도"),
+    GANGWON(32, "강원도"),
+    CHUNGBUK(33, "충청북도"),
+    CHUNGNAM(34, "충청남도"),
+    GYEONGBUK(35, "경상북도"),
+    GYEONGNAM(36, "경상남도"),
+    JEOLLABUK(37, "전라북도"),
+    JEOLLANAM(38, "전라남도"),
+    JEJU(39, "제주도");
+
+    private final int code;
+    private final String name;
+
+    Region(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
