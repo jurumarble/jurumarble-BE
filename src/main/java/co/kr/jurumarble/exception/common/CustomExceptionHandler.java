0f450fe800da7b46ecd81525c539f5ac7cd35678
jonghyunhub <chuwhdgus@gmail.com>
2023-08-11 02:01:10 +0900

    fix : HttpStatus 도 변환되도록 변경

--- src/main/java/co/kr/jurumarble/exception/common/CustomExceptionHandler.java
+++ src/main/java/co/kr/jurumarble/exception/common/CustomExceptionHandler.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ExceptionMessage> handle(CustomException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        int statusCode = e.getStatus().getStatusCode();
+        return ResponseEntity.status(HttpStatus.valueOf(statusCode))
                 .body(ExceptionMessage.of(e.getStatus(), e.getMessage()));
     }
 
