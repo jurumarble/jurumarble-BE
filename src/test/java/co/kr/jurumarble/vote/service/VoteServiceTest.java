643cb2da2a1893d2c9b6d90a84e825b959d6bdbc
jonghyunhub <chuwhdgus@gmail.com>
2023-08-10 20:27:19 +0900

    test : 전통주 투표 생성 서비스 테스트 작성

--- src/test/java/co/kr/jurumarble/vote/service/VoteServiceTest.java
+++ src/test/java/co/kr/jurumarble/vote/service/VoteServiceTest.java
@@ -4,11 +4,13 @@ import co.kr.jurumarble.user.domain.User;
 import co.kr.jurumarble.user.repository.UserRepository;
 import co.kr.jurumarble.vote.domain.Vote;
 import co.kr.jurumarble.vote.domain.VoteContent;
+import co.kr.jurumarble.vote.domain.VoteDrinkContent;
 import co.kr.jurumarble.vote.domain.VoteGenerator;
 import co.kr.jurumarble.vote.enums.VoteType;
 import co.kr.jurumarble.vote.repository.BookmarkRepository;
 import co.kr.jurumarble.vote.repository.VoteRepository;
 import co.kr.jurumarble.vote.repository.VoteResultRepository;
+import co.kr.jurumarble.vote.service.request.CreateDrinkVoteServiceRequest;
 import co.kr.jurumarble.vote.service.request.CreateNormalVoteServiceRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,7 +40,7 @@ class VoteServiceTest {
     @Mock
     private VoteGenerator voteGenerator;
 
-    @DisplayName("투표를 생성한다.")
+    @DisplayName("일반 투표를 생성한다.")
     @Test
     void createVote() {
         // given
@@ -66,6 +68,36 @@ class VoteServiceTest {
         voteService.createNormalVote(request, userId);
 
         // then
-        verify(voteGenerator, times(1)).createVote(vote, voteContent);
+        verify(voteGenerator, times(1)).createNormalVote(vote, voteContent);
+    }
+
+
+    @DisplayName("전통주 투표를 생성한다.")
+    @Test
+    void createDrinkVote() {
+        // given
+        Long userId = 1L;
+
+        User user = User.builder()
+                .build();
+
+        CreateDrinkVoteServiceRequest request = CreateDrinkVoteServiceRequest.builder()
+                .title("투표 제목")
+                .drinkIdA(1L)
+                .drinkIdB(2L)
+                .voteType(VoteType.DRINK)
+                .build();
+        VoteDrinkContent voteDrinkContent = request.toVoteDrinkContent();
+
+        Vote vote = request.toVote(userId);
+
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // when
+        voteService.createDrinkVote(request, userId);
+
+        // then
+        verify(voteGenerator, times(1)).createDrinkVote(vote, voteDrinkContent);
     }
 }
