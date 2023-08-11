e896bb61249d921e89e789553f7bf02d2f8296d8
wellbeing-dough <kdw990202@naver.com>
2023-08-12 03:53:35 +0900

    fix : 필요없는 주석 제거

--- src/main/java/co/kr/jurumarble/vote/repository/VoteEntityRepositoryImpl.java
+++ src/main/java/co/kr/jurumarble/vote/repository/VoteEntityRepositoryImpl.java
@@ -245,33 +245,4 @@ public class VoteEntityRepositoryImpl implements VoteEntityRepository {
         return recommendTitle;
     }
 
-//    @Override
-//    public List<String> findByTitleContains(String keyword) {
-//
-//        List<Tuple> findVotesOrderByPopularTuples = getRecommendVotesTupleOrderByPopular(keyword);
-//
-//        List<Long> voteIds = getVoteIdsFromFindVotes(findVotesOrderByPopularTuples);
-//
-//        List<VoteContent> voteContents = findVoteContentsByVoteIds(voteIds); //하나의 vote에서 두개의 voteContent 찾아야 함
-//
-//        Map<Long, VoteContent> voteContentsMap = voteContents.stream()  // List를 순회하면 성능이 안나오므로 <voteId, VoteConent> 로 이루어진 Map을 만듬
-//                .collect(Collectors.toMap(VoteContent::getVoteId, voteContent -> voteContent));// ex) <1, {voteContent}>
-//
-//        List<NormalVoteData> normalVoteData = getFindVoteListDatas(findVotesOrderByPopularTuples, voteContentsMap);
-//
-//        long totalCount = getVoteTotalCount();
-//
-//    }
-//
-//    private List<Tuple> getRecommendVotesTupleOrderByPopular(String keyword) {
-//        return jpaQueryFactory
-//                .select(vote, voteResult.id.count())
-//                .from(vote)
-//                .leftJoin(voteResult).on(vote.id.eq(voteResult.voteId))
-//                .where(vote.title.like(keyword + "%"))
-//                .groupBy(vote.id)
-//                .orderBy(voteResult.id.count().desc())
-//                .fetch();
-//    }
-
 }
