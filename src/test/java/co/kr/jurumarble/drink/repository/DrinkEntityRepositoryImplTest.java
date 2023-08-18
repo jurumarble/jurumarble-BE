b63ca8e5e2dccd123cdcc16a37012806d79b87ae
jonghyunhub <chuwhdgus@gmail.com>
2023-08-19 04:04:10 +0900

    test : 7월 27일부터 8월 3일까지 일주일간 즐겼어요가 많은 전통주를 조회한다.

--- src/test/java/co/kr/jurumarble/drink/repository/DrinkEntityRepositoryImplTest.java
+++ src/test/java/co/kr/jurumarble/drink/repository/DrinkEntityRepositoryImplTest.java
@@ -33,9 +33,9 @@ class DrinkEntityRepositoryImplTest {
     @Autowired
     private EntityManager entityManager;
 
-    @DisplayName("")
+    @DisplayName("8월 3일부터 8월 10일까지 일주일간 즐겼어요가 많은 전통주를 조회한다.")
     @Test
-    void test() {
+    void getHotDrinks1() {
         // given
         PageRequest of = PageRequest.of(0, 3);
         LocalDateTime testTime = LocalDateTime.of(2023, 8, 10, 7, 15);
@@ -50,6 +50,26 @@ class DrinkEntityRepositoryImplTest {
                         tuple(12L, "느린마을 막걸리", 3L),
                         tuple(2L, "인천 생 소성주", 2L),
                         tuple(30L, "호랑이 생 막걸리", 2L)
+                );
+    }
+
+    @DisplayName("7월 27일부터 8월 3일까지 일주일간 즐겼어요가 많은 전통주를 조회한다.")
+    @Test
+    void getHotDrinks2() {
+        // given
+        PageRequest of = PageRequest.of(0, 3);
+        LocalDateTime testTime = LocalDateTime.of(2023, 8, 3, 7, 15);
+
+        // when
+        Slice<HotDrinkData> hotDrinks = drinkEntityRepository.getHotDrinks(of, testTime);
+
+        // then
+        assertThat(hotDrinks).hasSize(3)
+                .extracting("drinkId", "name", "enjoyedCount")
+                .containsExactly(
+                        tuple(3L, "송명섭 막걸리", 2L),
+                        tuple(11L, "월매 쌀막걸리", 2L),
+                        tuple(20L, "장수 생 막걸리_750ml", 2L)
                 );
     }
 }
