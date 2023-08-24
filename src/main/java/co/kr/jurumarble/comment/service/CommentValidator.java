956d7719d0bd5c450679378847a36cec55b50a1e
alsduq1117 <alsduq1117@naver.com>
2023-08-24 21:44:13 +0900

    refactor: CommentValidator 클래스 책임 분리

---
+++ src/main/java/co/kr/jurumarble/comment/service/CommentValidator.java
@@ -0,0 +1,41 @@
+package co.kr.jurumarble.comment.service;
+
+import co.kr.jurumarble.comment.domain.Comment;
+import co.kr.jurumarble.comment.repository.CommentRepository;
+import co.kr.jurumarble.comment.service.request.CreateCommentServiceRequest;
+import co.kr.jurumarble.exception.comment.CommentNotBelongToUserException;
+import co.kr.jurumarble.exception.comment.CommentNotFoundException;
+import co.kr.jurumarble.exception.comment.NestedCommentNotAllowedException;
+import co.kr.jurumarble.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentValidator {
+
+    private final CommentRepository commentRepository;
+
+    public void validateCommentBelongsToUser(Comment comment, User user) {
+        if (!commentRepository.existsByIdAndUser(comment.getId(), user)) {
+            throw new CommentNotBelongToUserException();
+        }
+    }
+
+    public Comment checkParentComment(CreateCommentServiceRequest request) {
+        if (request.getParentId() == null) {
+            return null;
+        }
+        return commentRepository.findById(request.getParentId())
+                .orElseThrow(() -> new CommentNotFoundException());
+    }
+
+    public void checkNestedCommentAllowed(Comment parentComment) {
+        if (parentComment != null && parentComment.getParent() != null) {
+            throw new NestedCommentNotAllowedException();
+        }
+    }
+
+
+
+}
