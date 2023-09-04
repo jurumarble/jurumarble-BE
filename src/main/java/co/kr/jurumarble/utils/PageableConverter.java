4f85f681d3b847e2d5461496415cdf04a9ecff39
jonghyunhub <chuwhdgus@gmail.com>
2023-09-04 14:57:03 +0900

    fix : 페이지네이션시 리스트 갯수보다 큰 요청을 보내면 생기는 버그 수장

--- src/main/java/co/kr/jurumarble/utils/PageableConverter.java
+++ src/main/java/co/kr/jurumarble/utils/PageableConverter.java
@@ -4,20 +4,27 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PageableConverter {
 
     public <T> SliceImpl<T> convertListToSlice(List<T> list, int pageNum, int pageSize) {
-        boolean hasNext = false;
+        int start = pageNum * pageSize;
+        int end = Math.min((pageNum + 1) * pageSize, list.size());
 
-        if (list.size() > pageNum) {
-            hasNext = true;
-            list = list.subList(0, pageSize);
+        List<T> subList = new ArrayList<>();
+
+        if (start < list.size()) {
+            subList = list.subList(start, end);
         }
 
+        boolean hasNext = end < list.size();
+
         PageRequest pageRequest = PageRequest.of(pageNum, pageSize);
-        return new SliceImpl<T>(list, pageRequest, hasNext);
+
+        return new SliceImpl<>(subList, pageRequest, hasNext);
     }
+
 }
