f0d1fc1625ce3c561630b59581fe48318c92ee74
jonghyunhub <chuwhdgus@gmail.com>
2023-09-04 19:18:05 +0900

    refact : 에러 메시지 수정

--- src/main/java/co/kr/jurumarble/exception/vote/VoteNotFoundException.java
+++ src/main/java/co/kr/jurumarble/exception/vote/VoteNotFoundException.java
@@ -5,7 +5,7 @@ import co.kr.jurumarble.exception.common.CustomException;
 
 public class VoteNotFoundException extends CustomException {
 
-    private static final String message = "해당 id를 가진 투표가 없습니다. 아이디 값을 다시 한번 확인하세요.";
+    private static final String message = "해당 투표 id와 연결된 투표 컨텐츠가 없습니다. 아이디 값을 다시 한번 확인하세요.";
     private final StatusEnum status;
 
     public VoteNotFoundException() {
