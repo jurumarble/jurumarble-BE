f9c4bbbb7c3c9c7ac2c015bed05e67c15d284074
wellbeing-dough <kdw990202@naver.com>
2023-09-04 23:14:07 +0900

    docs : 스웨거 문서 수정

--- src/main/java/co/kr/jurumarble/vote/controller/VoteController.java
+++ src/main/java/co/kr/jurumarble/vote/controller/VoteController.java
@@ -32,14 +32,14 @@ import java.util.List;
 public class VoteController {
     private final VoteService voteService;
 
-    @Operation(summary = "일반 투표 생성", description = "헤더에 토큰 담고, 바디에 {title, titleA, titleB, imageA, imageB, filteredGender, filteredAge, filteredMbti} json 형식으로 보내주시면 됩니다.")
+    @Operation(summary = "일반 투표 생성", description = "헤더에 토큰 담고, 바디에 {title, titleA, titleB, imageA, imageB} json 형식으로 보내주시면 됩니다.")
     @PostMapping("/normal")
     public ResponseEntity createNormalVote(@Valid @RequestBody CreateNormalVoteRequest request, @RequestAttribute Long userId) {
         voteService.createNormalVote(request.toServiceRequest(), userId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @Operation(summary = "전통주 투표 생성", description = "헤더에 토큰 담고, 바디에 {title, titleA, titleB, imageA, imageB, filteredGender, filteredAge, filteredMbti} json 형식으로 보내주시면 됩니다.")
+    @Operation(summary = "전통주 투표 생성", description = "헤더에 토큰 담고, 바디에 {title, drinkAId, drinkBId} json 형식으로 보내주시면 됩니다.")
     @PostMapping("/drink")
     public ResponseEntity createDrinkVote(@Valid @RequestBody CreateDrinkVoteRequest request, @RequestAttribute Long userId) {
         voteService.createDrinkVote(request.toServiceRequest(), userId);
