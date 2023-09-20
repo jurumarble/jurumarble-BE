e1f0e50af01008409e4bd702d33ed3682a807ad6
alsduq1117 <alsduq1117@naver.com>
2023-09-20 21:03:59 +0900

    feat : 알림 기본 기능 commit

---
+++ src/main/java/co/kr/jurumarble/notification/controller/NotificationController.java
@@ -0,0 +1,26 @@
+package co.kr.jurumarble.notification.controller;
+
+import co.kr.jurumarble.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @GetMapping(value = "/subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@PathVariable Long id) {
+        return notificationService.subscribe(id);
+    }
+
+    @PostMapping("/send-data/{id}")
+    public void sendData(@PathVariable Long id) {
+        notificationService.notify(id, "data");
+    }
+}
