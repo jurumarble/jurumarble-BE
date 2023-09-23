cf61529ff8f684548f2fc87c79dafba67a1673d4
wellbeing-dough <kdw990202@naver.com>
2023-09-24 00:53:41 +0900

    feat : controller 반환값 재정립

--- src/main/java/co/kr/jurumarble/vote/controller/VoteController.java
+++ src/main/java/co/kr/jurumarble/vote/controller/VoteController.java
@@ -33,14 +33,14 @@ public class VoteController {
 
     @Operation(summary = "일반 투표 생성", description = "헤더에 토큰 담고, 바디에 {title, titleA, titleB, imageA, imageB} json 형식으로 보내주시면 됩니다.")
     @PostMapping("/normal")
-    public ResponseEntity createNormalVote(@Valid @RequestBody CreateNormalVoteRequest request, @RequestAttribute Long userId) {
+    public ResponseEntity<HttpStatus> createNormalVote(@Valid @RequestBody CreateNormalVoteRequest request, @RequestAttribute Long userId) {
         voteService.createNormalVote(request.toServiceRequest(), userId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Operation(summary = "전통주 투표 생성", description = "헤더에 토큰 담고, 바디에 {title, drinkAId, drinkBId} json 형식으로 보내주시면 됩니다.")
     @PostMapping("/drink")
-    public ResponseEntity createDrinkVote(@Valid @RequestBody CreateDrinkVoteRequest request, @RequestAttribute Long userId) {
+    public ResponseEntity<HttpStatus> createDrinkVote(@Valid @RequestBody CreateDrinkVoteRequest request, @RequestAttribute Long userId) {
         voteService.createDrinkVote(request.toServiceRequest(), userId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
@@ -90,35 +90,35 @@ public class VoteController {
 
     @Operation(summary = "일반 투표 수정", description = "파라미터에 voteId, 바디에 {title, detail, titleA, titleB} json 형식으로 보내주시면 됩니다.")
     @PutMapping("/{voteId}/normal")
-    public ResponseEntity updateNormalVote(@PathVariable("voteId") Long voteId, @RequestBody UpdateNormalVoteRequest request, @RequestAttribute Long userId) {
+    public ResponseEntity<HttpStatus> updateNormalVote(@PathVariable("voteId") Long voteId, @RequestBody UpdateNormalVoteRequest request, @RequestAttribute Long userId) {
         voteService.updateNormalVote(request.toServiceRequest(voteId, userId));
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @Operation(summary = "전통주 투표 수정", description = "파라미터에 voteId, 바디에 {title, detail, titleA, titleB} json 형식으로 보내주시면 됩니다.")
     @PutMapping("/{voteId}/drink")
-    public ResponseEntity updateDrinkVote(@PathVariable("voteId") Long voteId, @RequestBody UpdateDrinkVoteRequest request, @RequestAttribute Long userId) {
+    public ResponseEntity<HttpStatus> updateDrinkVote(@PathVariable("voteId") Long voteId, @RequestBody UpdateDrinkVoteRequest request, @RequestAttribute Long userId) {
         voteService.updateDrinkVote(request.toServiceRequest(voteId, userId));
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @Operation(summary = "투표 삭제", description = "헤더에 토큰 담고, 파라미터에 voteId 보내주시면 됩니다")
     @DeleteMapping("/{voteId}/")
-    public ResponseEntity deleteVote(@PathVariable("voteId") Long voteId, @RequestAttribute Long userId) {
+    public ResponseEntity<HttpStatus> deleteVote(@PathVariable("voteId") Long voteId, @RequestAttribute Long userId) {
         voteService.deleteVote(voteId, userId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @Operation(summary = "투표 참여", description = "헤더에 토큰담고, 파라미터에 voteId, 바디에 {choice} json 형식으로 보내주시면 됩니다.")
     @PostMapping("/{voteId}/vote")
-    public ResponseEntity doVote(@RequestBody DoVoteRequest request, @PathVariable("voteId") Long voteId, @RequestAttribute Long userId) {
+    public ResponseEntity<HttpStatus> doVote(@RequestBody DoVoteRequest request, @PathVariable("voteId") Long voteId, @RequestAttribute Long userId) {
         voteService.doVote(request.toService(userId, voteId));
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @Operation(summary = "투표 검색어 추천", description = "파라미터에 keyword, category 보내주시면 됩니다.")
     @GetMapping("/recommend")
-    public ResponseEntity recommendVote(@RequestParam String keyword, @RequestParam int recommendCount) {
+    public ResponseEntity<GetVoteRecommendListResponse> recommendVote(@RequestParam String keyword, @RequestParam int recommendCount) {
         List<String> voteRecommendListData = voteService.getRecommendVoteList(keyword, recommendCount);
         return new ResponseEntity(new GetVoteRecommendListResponse(voteRecommendListData), HttpStatus.OK);
     }
