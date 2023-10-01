2fadecd7290ec57b8cd407da03432e0a5397c5b5
jonghyunhub <chuwhdgus@gmail.com>
2023-10-01 21:42:09 +0900

    refact : VoteAction 맴버 클래스 타입으로 변경

--- src/main/java/co/kr/jurumarble/vote/enums/VoteType.java
+++ src/main/java/co/kr/jurumarble/vote/enums/VoteType.java
@@ -1,25 +1,24 @@
 package co.kr.jurumarble.vote.enums;
 
 import co.kr.jurumarble.common.enums.EnumModel;
-import co.kr.jurumarble.utils.SpringContext;
 import co.kr.jurumarble.vote.domain.DrinkVoteAction;
 import co.kr.jurumarble.vote.domain.NormalVoteAction;
 import co.kr.jurumarble.vote.domain.VoteAction;
 import co.kr.jurumarble.vote.dto.request.VoteWithPostedUserData;
-import co.kr.jurumarble.vote.repository.VoteContentRepository;
-import co.kr.jurumarble.vote.repository.VoteDrinkContentRepository;
 import co.kr.jurumarble.vote.repository.dto.VoteWithPostedUserCommonData;
+import org.springframework.context.ApplicationContext;
 
 public enum VoteType implements EnumModel {
-    NORMAL("일반투표", new NormalVoteAction(SpringContext.getBean(VoteContentRepository.class))),
-    DRINK("전통주투표", new DrinkVoteAction(SpringContext.getBean(VoteDrinkContentRepository.class)));
+    NORMAL("일반투표", NormalVoteAction.class),
+    DRINK("전통주투표", DrinkVoteAction.class);
+
 
     private final String value;
-    private final VoteAction action;
+    private final Class<? extends VoteAction> actionClass;
 
-    VoteType(String value, VoteAction action) {
+    VoteType(String value, Class<? extends VoteAction> actionClass) {
         this.value = value;
-        this.action = action;
+        this.actionClass = actionClass;
     }
 
     @Override
@@ -32,7 +31,10 @@ public enum VoteType implements EnumModel {
         return value;
     }
 
-    public VoteWithPostedUserData execute(Long voteId, VoteWithPostedUserCommonData voteCommonData) {
+    public VoteWithPostedUserData execute(Long voteId,
+                                          VoteWithPostedUserCommonData voteCommonData,
+                                          ApplicationContext context) {
+        VoteAction action = context.getBean(actionClass);
         return action.getVoteWithPostedUserData(voteId, voteCommonData);
     }
 }
