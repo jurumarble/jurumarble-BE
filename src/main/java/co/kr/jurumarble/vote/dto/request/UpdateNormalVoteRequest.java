47a8860a0caa748be0f373716b216f816bf05cfd
jonghyunhub <chuwhdgus@gmail.com>
2023-10-01 15:52:13 +0900

    refact : Request dto 패키지 구조 변경

--- src/main/java/co/kr/jurumarble/vote/dto/request/UpdateNormalVoteRequest.java
+++ src/main/java/co/kr/jurumarble/vote/dto/request/UpdateNormalVoteRequest.java
@@ -1,6 +1,6 @@
 package co.kr.jurumarble.vote.dto.request;
 
-import co.kr.jurumarble.vote.service.UpdateNormalVoteServiceRequest;
+import co.kr.jurumarble.vote.service.request.UpdateNormalVoteServiceRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
