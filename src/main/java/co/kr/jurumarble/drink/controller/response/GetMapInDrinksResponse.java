2545942218af284d90e6c80423524fc602d2c1b9
wellbeing-dough <kdw990202@naver.com>
2023-10-02 16:15:43 +0900

    feat : 술 지도에 이미지 추가

--- src/main/java/co/kr/jurumarble/drink/controller/response/GetMapInDrinksResponse.java
+++ src/main/java/co/kr/jurumarble/drink/controller/response/GetMapInDrinksResponse.java
@@ -10,12 +10,14 @@ public class GetMapInDrinksResponse {
     private String region;
     private Double latitude;
     private Double longitude;
+    private String image;
 
-    public GetMapInDrinksResponse(Long drinkId, String name, String region, Double latitude, Double longitude) {
+    public GetMapInDrinksResponse(Long drinkId, String name, String region, Double latitude, Double longitude, String image) {
         this.drinkId = drinkId;
         this.name = name;
         this.region = region;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.image = image;
     }
 }
--- src/main/java/co/kr/jurumarble/drink/domain/dto/MapInDrinkData.java
+++ src/main/java/co/kr/jurumarble/drink/domain/dto/MapInDrinkData.java
@@ -9,16 +9,18 @@ public class MapInDrinkData {
     private String region;
     private Double latitude;
     private Double longitude;
+    private String image;
 
-    public MapInDrinkData(Long drinkId, String name, String region, Double latitude, Double longitude) {
+    public MapInDrinkData(Long drinkId, String name, String region, Double latitude, Double longitude, String image) {
         this.drinkId = drinkId;
         this.name = name;
         this.region = region;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.image = image;
     }
 
     public GetMapInDrinksResponse toMapInDrinksResponse() {
-        return new GetMapInDrinksResponse(drinkId, name, region, latitude, longitude);
+        return new GetMapInDrinksResponse(drinkId, name, region, latitude, longitude, image);
     }
 }
